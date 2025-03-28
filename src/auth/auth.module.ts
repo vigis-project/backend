86e31fe7dabd8550174b767510c10fc77fe36577
autriz <explayinformer8519@gmail.com>
2025-03-28 15:05:01 +0400

    Удалил ключ-заглушку

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 
@@ -12,7 +11,7 @@ import { JwtModule } from '@nestjs/jwt';
 	imports: [
 		forwardRef(() => UsersModule),
 		JwtModule.register({
-			secret: process.env.JWT_PRIVATE_KEY || 'SUPER_MEGA_SECRET',
+			secret: process.env.JWT_PRIVATE_KEY,
 			signOptions: {
 				expiresIn: process.env.JWT_LIFE_TIME || '24h'
 			}
